bfb8776813d77ab960a4e6bd6ba98569d6b31cfb
DESKTOP-9ABPB02\pzuca <pierre.zucalli@e-artsup.net>
2019-02-01 10:41:54 +0100

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public bool isMoving = false;
+    [SerializeField] float moveSpeed;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        Vector3 velocity = new Vector3(xInput,0f,yInput);
+            
+        if (xInput != 0 || yInput != 0)
+        {
+            isMoving = true;
+            rb.velocity = velocity.normalized*moveSpeed * Time.fixedDeltaTime;
+        }
+        else
+        {
+            isMoving = false;
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
