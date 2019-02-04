d8d6e8487342c2590c54e40af1221e92f219e282
Kerian <kerian.simon@e-artsup.net>
2019-02-04 16:26:50 +0100

    add player dash without collisions

---
+++ Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    public float maxDashTime;
+    public float dashSpeed;
+    public float dashStoppingSpeed;
+
+    private float currentDashTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDashTime = maxDashTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Dash
+        if (Input.GetButtonDown("Fire2"))
+        {
+            currentDashTime = 0f;
+        }
+        if(currentDashTime < maxDashTime)
+        {
+            this.transform.position += new Vector3(Input.GetAxis("Horizontal") * dashSpeed, 0.0f, Input.GetAxis("Vertical") * dashSpeed);
+            currentDashTime += dashStoppingSpeed;
+        }
+    }
+}
