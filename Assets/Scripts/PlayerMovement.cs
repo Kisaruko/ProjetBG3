a2d6a6fcd63f34c70d302975bbbca2acb512c081
DESKTOP-9ABPB02\pzuca <pierre.zucalli@e-artsup.net>
2019-02-06 16:48:18 +0100

    shaders and test avaible for different options of shooting

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,13 +20,26 @@ public class PlayerMovement : MonoBehaviour
     {
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
+        float xInput2 = Input.GetAxis("Horizontal2");
+        float yInput2 = Input.GetAxis("Vertical2");
         Vector3 lookDirection = new Vector3(xInput,0f,yInput);
-            
+        Vector3 lookDirection2 = new Vector3(xInput2,0f,yInput2);
+
+
         if (xInput != 0 || yInput != 0)
         {
-            isMoving = true;
-            transform.rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
-            rb.velocity = transform.forward*moveSpeed * Time.fixedDeltaTime;
+            if (GetComponent<Shoot>().shootWithTrigger == true)
+            {
+                isMoving = true;
+                transform.rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+                rb.velocity = transform.forward * moveSpeed * Time.fixedDeltaTime;
+            }
+            if(GetComponent<Shoot>().shootWithJoystick == true || GetComponent<Shoot>().shootWithTriggerAndJoystick== true)
+            {
+                isMoving = true;
+                transform.rotation = Quaternion.LookRotation(lookDirection2, Vector3.up);
+                rb.velocity = new Vector3(xInput, 0f, yInput).normalized * moveSpeed * Time.fixedDeltaTime;
+            }
         }
         else
         {
