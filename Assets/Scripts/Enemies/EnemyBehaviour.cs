218bb538685313ac51be6eb2650e6a08324fabab
DESKTOP-9ABPB02\pzuca <pierre.zucalli@e-artsup.net>
2019-02-21 15:57:50 +0100

    Add new texture for shader, distance dissolvemat and enemy basic attack behaviour

--- Assets/Scripts/Enemies/EnemyBehaviour.cs
+++ Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -8,6 +8,10 @@ public class EnemyBehaviour : MonoBehaviour
     public bool playerIsInRange = false;
     public float moveSpeed;
     private GameObject player;
+    private float minDistanceToAttack =3f;
+    public float attackRange;
+    public float stength;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -20,5 +24,33 @@ public class EnemyBehaviour : MonoBehaviour
             transform.LookAt(player.transform);
             rb.velocity = (transform.forward.normalized) * moveSpeed;
         }
+        Attack();
+    }
+
+    void Attack()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if (minDistanceToAttack > distanceToPlayer && playerIsInRange == true)
+        {
+            playerIsInRange = false;
+            StartCoroutine("Attacking");
+        }
+    }
+    IEnumerator Attacking()
+    {
+        yield return new WaitForSeconds(0.5f);
+        foreach (Collider hitcol in Physics.OverlapSphere(transform.position + transform.forward, 1f))
+        {
+            if (hitcol.gameObject.tag == "Player")
+            {
+               // hitcol.gameObject.GetComponent<PlayerBehaviour>().LostLifePoint(strength);
+            }
+        }
+    
+        Debug.Log("Attack!!");
+        yield return new WaitForSeconds(1f);
+        playerIsInRange = true;
+        
+
     }
 }
