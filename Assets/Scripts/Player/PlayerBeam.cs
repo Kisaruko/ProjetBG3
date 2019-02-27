fbcf7288cad5cbddc9daeaef0baf6c30671ecf8d
DESKTOP-9ABPB02\pzuca <pierre.zucalli@e-artsup.net>
2019-02-27 17:50:46 +0100

    Add ray in front of player when he aim for feedback purpose

--- Assets/Scripts/Player/PlayerBeam.cs
+++ Assets/Scripts/Player/PlayerBeam.cs
@@ -15,7 +15,6 @@ public class PlayerBeam : MonoBehaviour
     private bool alreadyInstantiate = false;
     public GameObject loadingFx;
     public GameObject loadedFx;
-
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -26,10 +25,15 @@ public class PlayerBeam : MonoBehaviour
     }
     void ShootBeam()
     {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>(); // get le line renderer
         float xInput = Input.GetAxis("Horizontal2");
         float yInput = Input.GetAxis("Vertical2");
         if (xInput != 0 || yInput != 0) // si le joueur touche le joystick droit
         {
+
+            lineRenderer.SetVertexCount(2); // la ligne fait possede 2 vertex
+            lineRenderer.SetPosition(0, transform.position); // le premier point est sur le player
+            lineRenderer.SetPosition(1, transform.forward * 40 + transform.position); // le deuxieme point est devant le player + une range de 20
             loadingTime += Time.deltaTime; // augmente le temps de load en fonction du temps
             lastInput = new Vector3(xInput, 0f, yInput); // garde le dernier input en mémoire pour garder la rotation 
             isLoading = true; // le joueur est en train de load un tir
@@ -37,6 +41,7 @@ public class PlayerBeam : MonoBehaviour
         }
         else
         {
+            lineRenderer.SetVertexCount(0); // la ligne a 0 vertex, elle n'apparait donc pas
             if (loadingTime >= loadedTime) // si le joueur ne touche pas le joystick est-ce que le joueur a suffisament chargé pour tirer?
             {
                 if (GetComponent<PlayerBehaviour>().canShoot == true)// check si le joueur a assez de lumière pour tirer
