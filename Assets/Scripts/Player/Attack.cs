feea4fccd96bcd06a3bc7544cbe2a7cf25ba5132
DESKTOP-9ABPB02\pzuca <pierre.zucalli@e-artsup.net>
2019-03-05 18:08:48 +0100

    Add animations with attack, attack in 3 parts ,reduce speed while attacking ,3rd attack reload more light than 1st and 2nd.

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -5,43 +5,161 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     #region Variables
-    public GameObject fxAttack;
+    [Header("Values", order = 0)]
+    [Space(10, order = 1)]
     public int strength;
     public float range;
+    public float speedWhileAttacking;
+    [Header("Vfx", order = 0)]
+    [Space(10, order = 1)]
     public GameObject stealLightFx;
+    public GameObject fxAttack;
+    public PlayerMovement playermovement;
+    private Animator anim;
+    public int noOfClicks = 0;
+    //Time when last button was clicked
+    float lastClickedTime = 0;
+    //Delay between clicks for which clicks will be considered as combo
+    float maxComboDelay = 1;
+
     #endregion
 
-
     #region Main Methods
-    private void Update()
+    void Start()
     {
-        DoAttack();
+        anim = GetComponentInChildren<Animator>();
+        playermovement = GetComponentInParent<PlayerMovement>();
+        anim.SetBool("Attack1", false);
+        anim.SetBool("Attack2", false);
+        anim.SetBool("Attack3", false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Attack"))
+        {
+            OnButtonClick();
+        }
+        if (Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+            anim.SetBool("Attack1", false);
+            anim.SetBool("Attack2", false);
+            anim.SetBool("Attack3", false);
+        }
+        if(noOfClicks != 0)
+        {
+            playermovement.moveSpeed = speedWhileAttacking;
+        }
+    }
+    #endregion
+    #region Custom Methods
+    
+    #region InputDetection
+    void OnButtonClick()
+    {
+        //Record time of last button click
+        lastClickedTime = Time.time;
+        noOfClicks++;
+        if (noOfClicks == 1)
+        {
+            anim.SetBool("Attack1", true);
+        }
+        //limit/clamp no of clicks between 0 and 3 because you have combo for 3 clicks
+        noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
+        Debug.Log(noOfClicks);
+    }
+
+    public void ComboCheck1()
+    {
+        anim.SetBool("Attack1", false);
+        if (noOfClicks>= 2 )
+        {
+            anim.SetBool("Attack2", true);
+
+        }
+        else
+        {
+            noOfClicks = 0;
+        }
+    }
+    public void ComboCheck2()
+    {
+        anim.SetBool("Attack1", false);
+        anim.SetBool("Attack2", false);
+        if (noOfClicks >= 3)
+        {
+            anim.SetBool("Attack3", true);
+            noOfClicks = 0;
+
+        }
     }
     #endregion
 
-    #region Custom Methods
-    void DoAttack()
+    #region AttackBehaviour 
+    public void Attack1()
     {
-        if (Input.GetButtonDown("Fire1")) // Si le joueur appuie sur l'input d'attack
+        Instantiate(fxAttack, transform.position + transform.forward, Quaternion.identity); // Instantier le fx d'attaque
+        foreach (Collider hitcol in Physics.OverlapSphere(transform.position + transform.forward, range)) // Crée une sphère devant le joueur de radius range
         {
-            Instantiate(fxAttack, transform.position + transform.forward, Quaternion.identity); // Instantier le fx d'attaque
-            foreach (Collider hitcol in Physics.OverlapSphere(transform.position + transform.forward, range)) // Crée une sphère devant le joueur de radius range
+            if (hitcol.gameObject.tag == "Enemy") // pour chaque ennemi dans la sphere
             {
-                if (hitcol.gameObject.tag == "Enemy") // pour chaque ennemi dans la sphere
-                {
-                    //GetComponent<PlayerBehaviour>().RegenLifeOnCac(); // appelle la fonction regen de lumière
-                    hitcol.gameObject.GetComponent<EnemyLife>().LostLifePoint(strength); // appelle la fonction de perte de pdv du monstre, les dégats infligés sont égaux a strength
-                    Instantiate(stealLightFx, hitcol.transform.position, Quaternion.identity); // instantie le fx de vol de light
-                }
+                hitcol.gameObject.GetComponent<EnemyLife>().LostLifePoint(strength); // appelle la fonction de perte de pdv du monstre, les dégats infligés sont égaux a strength
+                Instantiate(stealLightFx, hitcol.transform.position, Quaternion.identity); // instantie le fx de vol de light
             }
         }
     }
-    void OnDrawGizmos()
+
+    public void Attack2()
+    {
+        Instantiate(fxAttack, transform.position + transform.forward, Quaternion.identity); // Instantier le fx d'attaque
+        foreach (Collider hitcol in Physics.OverlapSphere(transform.position + transform.forward, range)) // Crée une sphère devant le joueur de radius range
+        {
+            if (hitcol.gameObject.tag == "Enemy") // pour chaque ennemi dans la sphere
+            {
+                hitcol.gameObject.GetComponent<EnemyLife>().LostLifePoint(strength); // appelle la fonction de perte de pdv du monstre, les dégats infligés sont égaux a strength
+                Instantiate(stealLightFx, hitcol.transform.position, Quaternion.identity); // instantie le fx de vol de light
+            }
+        }
+    }
+
+    public void Attack3()
+    {
+        Instantiate(fxAttack, transform.position + transform.forward, Quaternion.identity); // Instantier le fx d'attaque
+        foreach (Collider hitcol in Physics.OverlapSphere(transform.position + transform.forward, range)) // Crée une sphère devant le joueur de radius range
+        {
+            if (hitcol.gameObject.tag == "Enemy") // pour chaque ennemi dans la sphere
+            {
+                hitcol.gameObject.GetComponent<EnemyLife>().LostLifePoint(strength+1); // appelle la fonction de perte de pdv du monstre, les dégats infligés sont égaux a strength
+                Instantiate(stealLightFx, hitcol.transform.position, Quaternion.identity); // instantie le fx de vol de light
+                Instantiate(stealLightFx, hitcol.transform.position, Quaternion.identity); // instantie le fx de vol de light
+            }
+        }
+    }
+
+    /*  void DoAttack()
+       {
+           if (Input.GetButtonDown("Attack")) // Si le joueur appuie sur l'input d'attack
+           {
+               Instantiate(fxAttack, transform.position + transform.forward, Quaternion.identity); // Instantier le fx d'attaque
+               foreach (Collider hitcol in Physics.OverlapSphere(transform.position + transform.forward, range)) // Crée une sphère devant le joueur de radius range
+               {
+                   if (hitcol.gameObject.tag == "Enemy") // pour chaque ennemi dans la sphere
+                   {
+                       hitcol.gameObject.GetComponent<EnemyLife>().LostLifePoint(strength); // appelle la fonction de perte de pdv du monstre, les dégats infligés sont égaux a strength
+                       Instantiate(stealLightFx, hitcol.transform.position, Quaternion.identity); // instantie le fx de vol de light
+                   }
+               }
+           }
+       }*/
+    #endregion
+    #endregion
+    #region Debug
+    private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
-            //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
-            Gizmos.DrawWireSphere(transform.position+ transform.forward, range);
+     //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
+    // Gizmos.DrawWireSphere(transform.position + m_Position, m_Radius);       
     }
     #endregion
 }
