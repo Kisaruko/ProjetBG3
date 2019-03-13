9db754c9d51dab8540d1bb9c46d985983f40f003
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-03-13 17:04:58 +0100

    clean code and comment

--- Assets/Scripts/Interactables/RandomSpawner.cs
+++ Assets/Scripts/Interactables/RandomSpawner.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class RandomSpawner : MonoBehaviour
 {
+    #region Variables
     public GameObject gameObjectToSpawn;
     public int nbObjectToSpawn;
     public float range = 1f;
     public bool spawnInTheAir = false;
+    #endregion
 
+    #region Methods
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player")) // si le joueur touche cet objet
@@ -18,11 +21,11 @@ public class RandomSpawner : MonoBehaviour
             {
                 if (spawnInTheAir == false) // Spawner au niveau du y du spawner
                 {
-                    spawnPos = new Vector3(Random.Range(transform.position.x + range, transform.position.x - range), transform.position.y, Random.Range(transform.position.z + range, transform.position.z - range));
+                    spawnPos = new Vector3(Random.Range(transform.position.x + range, transform.position.x - range), transform.position.y, Random.Range(transform.position.z + range, transform.position.z - range));// randomise la position du spawner aléatoirement dans la range sans Y
                 }
                 else // autoriser le a spawner au dessus du spawner
                 {
-                    spawnPos = new Vector3(Random.Range(transform.position.x + range, transform.position.x - range), Random.Range(transform.position.y + range, transform.position.y), Random.Range(transform.position.z + range, transform.position.z - range));
+                    spawnPos = new Vector3(Random.Range(transform.position.x + range, transform.position.x - range), Random.Range(transform.position.y + range, transform.position.y), Random.Range(transform.position.z + range, transform.position.z - range));// randomise la position du spawner aléatoirement dans la range avec +Y uniquement
                 }
                 Instantiate(gameObjectToSpawn, spawnPos, Quaternion.identity); // instantier un enemy sur le spawnpos              
             }
@@ -34,4 +37,5 @@ public class RandomSpawner : MonoBehaviour
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, range);
     }
+    #endregion
 }
--- Assets/Scripts/Interactables/SpawnerWithPoints.cs
+++ Assets/Scripts/Interactables/SpawnerWithPoints.cs
@@ -13,9 +13,9 @@ public class SpawnerWithPoints : MonoBehaviour
     #region Main Methods
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))// si le player declenche le spawn
         {
-            InvokeEnemies();
+            InvokeEnemies();// invoque ennemies
         }
     }
     private void OnDrawGizmosSelected()
@@ -28,28 +28,28 @@ public class SpawnerWithPoints : MonoBehaviour
 
     public List<Transform> GetSpawnersInRange()
     {
-        List<Transform> spawnPoints = new List<Transform>();
-        foreach(Collider spawnpoints in Physics.OverlapSphere(transform.position,detectionRange,spawners))
+        List<Transform> spawnPoints = new List<Transform>(); // new list
+        foreach(Collider spawnpoints in Physics.OverlapSphere(transform.position,detectionRange,spawners)) // répertorie tout les spawners dans la range de detection
         {
             spawnPoints.Add(spawnpoints.transform);// add un composant a la liste
         }
-        return spawnPoints;
+        return spawnPoints; // retourne la liste de tout les spawners dans la range
 
     }
     private void InvokeEnemies()
     {
-        List<Transform> touchedSpawners = GetSpawnersInRange();
-        Transform[] spawnerArray = touchedSpawners.ToArray();
+        List<Transform> touchedSpawners = GetSpawnersInRange(); // recupere la liste générée par l'autre fonction
+        Transform[] spawnerArray = touchedSpawners.ToArray(); // Transforme la liste en Tableau
         /*for (int i = 0; i < spawnerArray.Length; i++) // instantier un fx d'annonciation
         {
             Instantiate(//Fx annonciateur de chaos, spawnerArray[i].transform.position, Quaternion.identity);          
         }*/
-        for (int i = 0; i < spawnerArray.Length; i++)
+        for (int i = 0; i < spawnerArray.Length; i++) // Pour chaque spawnpoint dans le tableau
         {
-            Instantiate(gameObjectToSpawn, spawnerArray[i].transform.position, Quaternion.identity);
-            Destroy(spawnerArray[i].gameObject);
+            Instantiate(gameObjectToSpawn, spawnerArray[i].transform.position, Quaternion.identity); // instantie l'objet sur un spawnpoint
+            Destroy(spawnerArray[i].gameObject); // detruit le spawn point sur lequel l'objet vient d'etre instantié
         }
-        Destroy(this.gameObject);
+        Destroy(this.gameObject);// quand la boucle est finie, détruire le spawner
     }
     #endregion
 }
