ba32934ce48626503035e5a7458868d5801c2f6d
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-03-21 17:26:39 +0100

    Modif game manager, opti shader instance in image effect

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,9 @@ public class GameManager : MonoBehaviour
     public Material pencilMat;
     private Camera myCam;
     private ImageEffect imageeffect;
+    private float baseGradTresh;
+    private float baseOutlineTresh;
+
     void Awake()
     {
         _instance = this;
@@ -18,11 +21,36 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        myDeltaTime = Time.deltaTime;
+        myDeltaTime = 0.03f;
         myCam = Camera.main;
         imageeffect = myCam.GetComponent<ImageEffect>();
+
+        pencilMat.SetFloat("_GradThresh", 0.2f);
+        pencilMat.SetFloat("_OutLineTresh",20f);
+        baseGradTresh = pencilMat.GetFloat("_GradThresh");
+        baseOutlineTresh = pencilMat.GetFloat("_OutLineTresh");
+    }
+    private void Update()
+    {
+        #region lerping Shader
+        if (isTimeScaleAltered)
+        {
+            Time.timeScale = Mathf.MoveTowards(Time.timeScale, 1f, myDeltaTime * 0.1f);
+            pencilMat.SetFloat("_ColorThreshold", Mathf.MoveTowards(pencilMat.GetFloat("_ColorThreshold"), 1f, myDeltaTime * 6.27f));
+            pencilMat.SetFloat("_GradThresh", Mathf.MoveTowards(pencilMat.GetFloat("_GradThresh"), 0.03f, myDeltaTime * 1.98f));
+            pencilMat.SetFloat("_OutLineTresh", Mathf.MoveTowards(pencilMat.GetFloat("_OutLineTresh"), 3f, myDeltaTime * 198));
+
+        }
+        if (pencilMat.GetFloat("_ColorThreshold") != 0 && !isTimeScaleAltered)
+        {
+            pencilMat.SetFloat("_ColorThreshold", Mathf.MoveTowards(pencilMat.GetFloat("_ColorThreshold"), 0f, myDeltaTime * 0.95f));
+            pencilMat.SetFloat("_GradThresh", Mathf.MoveTowards(pencilMat.GetFloat("_GradThresh"), baseGradTresh, myDeltaTime * 0.3f));
+            pencilMat.SetFloat("_OutLineTresh", Mathf.MoveTowards(pencilMat.GetFloat("_OutLineTresh"), baseOutlineTresh, myDeltaTime * 30));
+        }
+        #endregion
     }
 
+    #region ImpactEffect
     public static void ShowAnImpact(float timeAlteredDuration)
     {
         _instance.StopAllCoroutines();
@@ -31,28 +59,18 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator ImpactEffect(float timeAlteredDuration)
     {
-        //CameraShake.Shake(0.1f, 0.2f);
         Time.timeScale = 0.01f;
-        imageeffect.enabled = true;
+        imageeffect.isFiltered = true;
         isTimeScaleAltered = true;
-        //yield return new WaitForSecondsRealtime(0.15f);
         yield return new WaitForSecondsRealtime(timeAlteredDuration);
         isTimeScaleAltered = false;
         Time.timeScale = 1f;
-        //imageeffect.enabled = false;
+        yield return new WaitForSecondsRealtime(0.3f);
+        imageeffect.isFiltered = false;
+
         StopCoroutine("ImpactEffect");
     }
-    private void Update()
-    {
-        if (isTimeScaleAltered)
-        {         
-            Time.timeScale = Mathf.MoveTowards(Time.timeScale, 1f, myDeltaTime * 0.1f);
-            pencilMat.SetFloat("_ColorThreshold", Mathf.MoveTowards(pencilMat.GetFloat("_ColorThreshold"), 1f, myDeltaTime *0.95f));
-        }
-        if(pencilMat.GetFloat("_ColorThreshold") !=0 && !isTimeScaleAltered)
-        {
-            pencilMat.SetFloat("_ColorThreshold", Mathf.MoveTowards(pencilMat.GetFloat("_ColorThreshold"), 0f, myDeltaTime * 0.95f));
-        }
-    }
+    #endregion
+
 
 }
