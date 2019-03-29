39a182dce03a3fffb088ac46ffee8184336a75d4
Hugo <kerdranvat.hugo@gmail.com>
2019-03-29 10:34:48 +0100

    Test des feuilles émissives sur l'arbre d'olivier / Test de fake volum light + scrolling noise / rangement sunshaft dans le dossier light et supression de dossier material vides

---
+++ Assets/Scripts/ScrollingTexture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingTexture : MonoBehaviour {
+    Material m_mat;
+    public float scrollXSpeed;
+    public float scrollYSpeed;
+    private Vector2 scrollSpeed;
+    private void Start()
+    {
+        m_mat = GetComponent<MeshRenderer>().material;
+        scrollSpeed = new Vector2(scrollXSpeed, scrollYSpeed);
+    }
+    void LateUpdate ()
+    {
+        m_mat.mainTextureOffset = scrollSpeed * Time.time;
+       
+	}
+}
