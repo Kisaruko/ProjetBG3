8ef41ce547c019772daa232b00e7e7d02250454f
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-05-10 09:47:15 +0200

    Classify scripts and WIP rock spawner and deadZone

---
+++ Assets/Scripts/Interactables/Spawners/SpawnerRock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerRock : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public float timeBeforeSpawn;
+    public float spawnRate;
+    public bool isSpawning = true;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", timeBeforeSpawn, spawnRate);
+    }
+    public void SpawnObject()
+    {
+        if(isSpawning)
+        {
+          GameObject clone = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+          Destroy(clone, 10f);
+        }
+    }
+}
