304bf37798b1496449dbb78730b8d9a6955d76ea
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-05-10 10:24:39 +0200

    add unity events for spawnerrock

--- Assets/Scripts/Interactables/ObjectsManagement/SpawnerRock.cs
+++ Assets/Scripts/Interactables/ObjectsManagement/SpawnerRock.cs
@@ -2,23 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerRock : MonoBehaviour
+public class SpawnerRock : ActivableObjects
 {
     public GameObject objectToSpawn;
     public float timeBeforeSpawn;
     public float spawnRate;
     public bool isSpawning = true;
     public float timeBeforeDestroyObject;
-    void Start()
+
+    public override void Activate()
     {
         InvokeRepeating("SpawnObject", timeBeforeSpawn, spawnRate);
     }
+    public override void Deactivate()
+    {
+        CancelInvoke();
+    }
+    void Start()
+    {
+        if (isSpawning)
+        {
+            InvokeRepeating("SpawnObject", timeBeforeSpawn, spawnRate);
+        }
+    }
     public void SpawnObject()
     {
-        if(isSpawning)
+        if (isSpawning)
         {
-          GameObject clone = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
-          Destroy(clone, timeBeforeDestroyObject);
+            GameObject clone = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+            Destroy(clone, timeBeforeDestroyObject);
         }
     }
 }
