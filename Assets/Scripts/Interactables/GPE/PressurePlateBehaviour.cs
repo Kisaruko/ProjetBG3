1a623855591ad64f312ccf89ad024676b330c9c6
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-05-28 09:49:21 +0200

    Rework light detection vfx part, chain reaction is every 2 frames, fix bugs with godray and pressure plate

--- Assets/Scripts/Interactables/GPE/PressurePlateBehaviour.cs
+++ Assets/Scripts/Interactables/GPE/PressurePlateBehaviour.cs
@@ -15,6 +15,8 @@ public class PressurePlateBehaviour : MonoBehaviour
 
     public GameObject multipleEntryDoor;
 
+    public int nbObjectOnThis = 0;
+
     [Header("Pressure Plate Variables")]
     public float pressureFactorY;
     private float startingPosY;
@@ -46,8 +48,8 @@ public class PressurePlateBehaviour : MonoBehaviour
                 {
                     associatedGameobject[i].GetComponent<ActivableDoorBehaviour>().Activate();
                 }*/
-
-                activateEvent.Invoke();
+                nbObjectOnThis += 1;
+                ExecuteAnimation();
             }
         }
         if(multipleEntryDoor != null)
@@ -75,8 +77,8 @@ public class PressurePlateBehaviour : MonoBehaviour
                 {
                     associatedGameobject[i].GetComponent<ActivableDoorBehaviour>().Deactivate();
                 }*/
-
-                deactivateEvent.Invoke();
+                nbObjectOnThis -= 1;
+                ExecuteAnimation();
             }
         }
         if (multipleEntryDoor != null)
@@ -110,6 +112,17 @@ public class PressurePlateBehaviour : MonoBehaviour
             activateEvent.Invoke();
         }
         else
+        {
+            deactivateEvent.Invoke();
+        }
+    }
+    private void ExecuteAnimation()
+    {
+        if (nbObjectOnThis > 0)
+        {
+            activateEvent.Invoke();
+        }
+        if (nbObjectOnThis <= 0)
         {
             deactivateEvent.Invoke();
         }
--- Assets/Scripts/Player/LightDetection.cs
+++ Assets/Scripts/Player/LightDetection.cs
@@ -5,23 +5,26 @@ using UnityEngine;
 public class LightDetection : MonoBehaviour
 {
     public float range;
-    public LayerMask mask;
+    public LayerMask ObjectsThatCanBeTouched;
     private Light light;
-   // public float rangeMultiplier;
+    // public float rangeMultiplier;
+    private Transform actualVfxTarget;
+
     public ParticleSystem ps;
     public LayerMask exceptionLayer;
     private BinaryLight binaryLight;
     [Header("Vfx Attributes", order = 0)]
     [Space(10, order = 1)]
-    public bool followTarget;
-    public float particlesSpeed;
-    ParticleSystem.Particle[] m_Particles;
-    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
-    ParticleSystem.NoiseModule noise;
-    ParticleSystem.TrailModule trail;
-    ParticleSystem.EmissionModule emission;
-    ParticleSystem.VelocityOverLifetimeModule velocityOverLifetime;
-    private GameObject particlesTarget;
+    /*  public bool followTarget;
+      public float particlesSpeed;
+      ParticleSystem.Particle[] m_Particles;
+      List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+      ParticleSystem.NoiseModule noise;
+      ParticleSystem.TrailModule trail;
+      ParticleSystem.EmissionModule emission;
+      ParticleSystem.VelocityOverLifetimeModule velocityOverLifetime;
+      private GameObject particlesTarget;*/
+    public GameObject vfxTransmission;
     public float stoppingRange=0.3f;
     private Rigidbody rb;
 
@@ -34,30 +37,42 @@ public class LightDetection : MonoBehaviour
     {
         binaryLight = GameObject.Find("Player").GetComponent<BinaryLight>();
         light = GetComponentInChildren<Light>();
-        emission = ps.emission;
-        emission.enabled = false;
+        //emission = ps.emission;
+        //emission.enabled = false;
         rb = GetComponent<Rigidbody>();
     }
     private void Update()
     {
-       // range = light.intensity * rangeMultiplier;
-        foreach (Collider hitcol in Physics.OverlapSphere(transform.position, range, mask)) // crée une sphere de detection
+        List<SwitchBehaviour> switchsList = new List<SwitchBehaviour>(); //crée une liste
+
+        // range = light.intensity * rangeMultiplier;
+        foreach (Collider hitcol in Physics.OverlapSphere(transform.position, range, ObjectsThatCanBeTouched)) // crée une sphere de detection
         {
             Vector3 toCollider = hitcol.transform.position - transform.position; // get le vecteur entre ennemi et player
             Ray ray = new Ray(transform.position, toCollider); // trace un rayon entre les deux
-            if (!Physics.Raycast(ray, toCollider.magnitude, ~mask)) // si le ray ne touche pas de mur
+            if (!Physics.Raycast(ray, toCollider.magnitude, ~ObjectsThatCanBeTouched)) // si le ray ne touche pas de mur
             {
                 if (hitcol.GetComponent<SwitchBehaviour>() != null)
                 {
-                    followTarget = true;
+
+                    // followTarget = true;
                     hitcol.GetComponent<SwitchBehaviour>().playerLight = this.gameObject;
                     hitcol.GetComponent<SwitchBehaviour>().Loading();
+
+                    SwitchBehaviour switchbehaviour = hitcol.GetComponent<SwitchBehaviour>();
+
                     if (hitcol.GetComponent<SwitchBehaviour>().isActivated == false)
                     {
-                        ps.trigger.SetCollider(0, hitcol.GetComponent<SphereCollider>());// get le collider du player qui doit détruire les particules
-
-                        particlesTarget = hitcol.gameObject;
-                        emission.enabled = true;
+                        switchsList.Add(switchbehaviour);
+                        int index = Random.Range(0, switchsList.Count);
+                        actualVfxTarget = switchsList[index].transform;
+                        GameObject clone = Instantiate(vfxTransmission, transform.position, transform.rotation);
+                        clone.GetComponent<SuckedLightBehaviour>().light = transform;
+                        clone.GetComponent<SuckedLightBehaviour>().isSucked = true;
+                        clone.GetComponent<SuckedLightBehaviour>().mobSuckingSpot = actualVfxTarget;
+                        /* ps.trigger.SetCollider(0, hitcol.GetComponent<SphereCollider>());// get le collider du player qui doit détruire les particules
+                         particlesTarget = hitcol.gameObject;
+                         emission.enabled = true;*/
                     }
                 }
                 if(hitcol.GetComponent<EmitWhenTrigger>() != null)
@@ -72,16 +87,24 @@ public class LightDetection : MonoBehaviour
                     }
                 }
             }
+            switchsList.Clear();
         }
 
-        if(followTarget)
+    /*    if(followTarget)
         {
             ParticlesGoToTarget();
-        }
+        }*/
     }
     private void FixedUpdate()
     {
         StopObject();
+    }
+    private void StopObject()
+    {
+        if (Physics.CheckSphere(transform.position, stoppingRange, ~exceptionLayer))
+        {
+            binaryLight.LightCanBeRegrabed();
+        }
     }
     private void OnDrawGizmosSelected()
     {
@@ -95,7 +118,7 @@ public class LightDetection : MonoBehaviour
     #endregion
     #region ParticlesMethods
 
-    public void StopFollow()
+  /*  public void StopFollow()
     {
         emission.enabled = false;
         followTarget = false;
@@ -117,19 +140,14 @@ public class LightDetection : MonoBehaviour
         {
             Vector3 newVelocity = m_Particles[i].position - particlesTarget.transform.position + Vector3.up*5; // Calcule la direction vers le joueur
             m_Particles[i].velocity = (newVelocity * particlesSpeed) * -1; // Set la velocité du particule concerné
-        }*/
+        }
         // Applique les changements au particule system
         ps.SetParticles(m_Particles, numParticlesAlive);
 
     }
+    */
 
-    private void StopObject()
-    {
-        if (Physics.CheckSphere(transform.position, stoppingRange,~exceptionLayer))
-        {
-            binaryLight.LightCanBeRegrabed();
-        }
-    }
+    /*
     void InitializeIfNeeded()
     {
         if (ps == null) // si le particle system n'est pas set
@@ -140,6 +158,6 @@ public class LightDetection : MonoBehaviour
 
         if (m_Particles == null || m_Particles.Length < ps.main.maxParticles) // Si pas de particules sont instantiées ou si le tableau de particule est inférieur au max de particules
             m_Particles = new ParticleSystem.Particle[ps.main.maxParticles]; // Set la taille de la list de particules au max des particules atteignable
-    }
+    }*/
     #endregion
 }
