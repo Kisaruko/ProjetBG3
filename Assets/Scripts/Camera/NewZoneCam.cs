cd1bb0aa10be6fd0bbf41ae48bc1903bf654d84f
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-06-04 17:42:29 +0200

    New spawner, new zoneCam  modifier, test new form of puzzle

--- Assets/Scripts/Camera/NewZoneCam.cs
+++ Assets/Scripts/Camera/NewZoneCam.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class CameraManager : MonoBehaviour
+public class NewZoneCam : MonoBehaviour
 {
     public float newCamHeight;
     public float newCamDistance;
     public float newCamAngle;
+    private float oldCamHeight;
+    private float oldCamDist;
+    private float oldCamAngle;
 
     public float timeToMoveCam;
 
@@ -16,18 +19,27 @@ public class CameraManager : MonoBehaviour
     private void Start()
     {
         cameraBehaviour = GameObject.FindGameObjectWithTag("MainCamera").GetComponentInParent<CameraBehaviour>();
+        oldCamHeight = cameraBehaviour.camHeight;
+        oldCamDist = cameraBehaviour.camDistance;
+        oldCamAngle = cameraBehaviour.xCamRotation;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //Si le joueur touche, ça set les valeurs de camera données au behaviour de la camera
-        if(other.CompareTag("Player"))
+        if (other.gameObject.layer == 11)
         {
             DOTween.To(() => cameraBehaviour.xCamRotation, x => cameraBehaviour.xCamRotation = x, newCamAngle, timeToMoveCam);
             DOTween.To(() => cameraBehaviour.camHeight, x => cameraBehaviour.camHeight = x, newCamHeight, timeToMoveCam);
-            //cameraBehaviour.camHeight = newCamHeight;
             DOTween.To(() => cameraBehaviour.camDistance, x => cameraBehaviour.camDistance = x, newCamDistance, timeToMoveCam);
-            //cameraBehaviour.camDistance = newCamDistance;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 11)
+        {
+            DOTween.To(() => cameraBehaviour.xCamRotation, x => cameraBehaviour.xCamRotation = x, oldCamAngle, timeToMoveCam);
+            DOTween.To(() => cameraBehaviour.camHeight, x => cameraBehaviour.camHeight = x, oldCamHeight, timeToMoveCam);
+            DOTween.To(() => cameraBehaviour.camDistance, x => cameraBehaviour.camDistance = x, oldCamDist, timeToMoveCam);
         }
     }
 }
---
+++ Assets/Scripts/Interactables/ObjectsManagement/SpawnerOneByOne.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerOneByOne : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public float checkTimeBeforeEachInvoke;
+    private GameObject clone;
+
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnObject", 1f, checkTimeBeforeEachInvoke);
+    }
+    private void SpawnObject()
+    {
+        if(clone == null)
+        {
+            clone = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+            //SecurityCheck
+            Destroy(clone, 100);
+        }
+    }
+}
