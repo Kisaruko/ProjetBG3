be7c25616f24473f339603f00a31430802b49ca4
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-06-07 12:21:44 +0200

    Modify speed playerController on moving animation,create moustique prefab and new scripts : rotateThings and moustiqueBehaviour

---
+++ Assets/Scripts/Interactables/MovingPlatforms/RotateThings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RotateThings : ActivableObjects
+{
+    private Transform thisTransform;
+    private Vector3 startRotation;
+    public Vector3 endRotation;
+
+    private void Start()
+    {
+        thisTransform = transform;
+        startRotation = transform.rotation.eulerAngles;
+    }
+    public override void Activate()
+    {
+        transform.DORotate(endRotation, 2f, RotateMode.Fast);
+    }
+    public override void Deactivate()
+    {
+        transform.DORotate(startRotation, 2f, RotateMode.Fast);
+    }
+}
