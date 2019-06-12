51277a14a4c6a98042bfc993f438b5bbc9e58d72
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-06-12 18:02:16 +0200

    Fix lightSanctuary

--- Assets/Scripts/Interactables/GPE/LightSanctuary.cs
+++ Assets/Scripts/Interactables/GPE/LightSanctuary.cs
@@ -6,10 +6,12 @@ public class LightSanctuary : MonoBehaviour
 {
     private GameObject playerLight;
     public bool getLightOnTrigger;
+
     private void Start()
     {
         playerLight = FindObjectOfType<LightManager>().gameObject;
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 11 && other.GetComponentInParent<BinaryLight>().gotLight == false)
@@ -21,7 +23,7 @@ public class LightSanctuary : MonoBehaviour
             else
             {
                 playerLight.transform.parent = null;
-                playerLight.transform.position = transform.position;
+                playerLight.transform.position = transform.position+Vector3.up;
             }
         }
     }
