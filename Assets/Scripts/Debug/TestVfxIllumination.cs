5c9640f0fddbd62b1308775552d5645d47f25fea
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-06-12 09:13:12 +0200

    New vfx Illumination iteration, new shader rippleEffect

---
+++ Assets/Scripts/Debug/TestVfxIllumination.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVfxIllumination : MonoBehaviour
+{
+    public GameObject vfxIllu;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(vfxIllu, transform.position, Quaternion.identity);
+        }
+    }
+}
