28e51a5c23bf5a6cc24648afcdc046f1fb30883e
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-06-13 15:58:08 +0200

    Modif lightsanctuary, update sandboxScene

--- Assets/Scripts/Interactables/GPE/LightSanctuary.cs
+++ Assets/Scripts/Interactables/GPE/LightSanctuary.cs
@@ -6,25 +6,52 @@ public class LightSanctuary : MonoBehaviour
 {
     private GameObject playerLight;
     public bool getLightOnTrigger;
+    public ParticleSystem feedBackPs;
+    private BinaryLight binarylight;
+    private ScaleOverTime scaleovertime;
 
     private void Start()
     {
         playerLight = FindObjectOfType<LightManager>().gameObject;
+        binarylight = FindObjectOfType<BinaryLight>();
+        InvokeRepeating("CheckIfPlayerGotLight", 0.1f, 1f);
+        scaleovertime = this.transform.parent.GetComponentInChildren<ScaleOverTime>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.layer == 11 && other.GetComponentInParent<BinaryLight>().gotLight == false)
         {
-            if (getLightOnTrigger)
+            if (Input.GetButtonDown("Attack"))
             {
-                other.gameObject.GetComponentInParent<BinaryLight>().GetLight();
+                feedBackPs.Stop();
+                scaleovertime.isScaling = false;
+                scaleovertime.SetScaling();
+                if (getLightOnTrigger)
+                {
+                    other.gameObject.GetComponentInParent<BinaryLight>().GetLight();
+                }
+                else
+                {
+                    playerLight.transform.parent = null;
+                    playerLight.transform.position = transform.position + Vector3.up;
+                }
             }
-            else
-            {
-                playerLight.transform.parent = null;
-                playerLight.transform.position = transform.position+Vector3.up;
-            }
+        }
+    }
+    private void CheckIfPlayerGotLight()
+    {
+        if(binarylight.gotLight)
+        {
+            feedBackPs.Stop();
+            scaleovertime.isScaling = false;
+            scaleovertime.SetScaling();
+        }
+        else
+        {
+            feedBackPs.Play();
+            scaleovertime.isScaling = true;
+            scaleovertime.SetScaling();
         }
     }
 }
