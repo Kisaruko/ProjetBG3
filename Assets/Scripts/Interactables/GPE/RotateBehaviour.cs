49d0c760082eab8db29d71a641741133234b28c2
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-06-14 14:39:27 +0200

    Rotate behaviour script

---
+++ Assets/Scripts/Interactables/GPE/RotateBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class RotateBehaviour : ActivableObjects
+{
+    [FormerlySerializedAs("m_EulerSpeed")]
+    public Vector3 EulerSpeed = Vector3.zero;
+    public bool isActivated;
+    void Update()
+    {
+        if (isActivated)
+        {
+            var euler = transform.rotation.eulerAngles;
+            euler += EulerSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(euler);
+        } 
+    }
+    public override void Activate()
+    {
+        isActivated = true;
+    }
+    public override void Deactivate()
+    {
+        isActivated = false;
+    }
+
+}
