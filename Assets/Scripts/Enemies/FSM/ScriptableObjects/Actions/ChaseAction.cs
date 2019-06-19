02be7c8628f7edcd82d0d7a0553fdca52eda90bc
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-06-19 11:55:55 +0200

    Color change when trashmob chase, and pulse, and spread vfx (PlaceHolder)

--- Assets/Scripts/Enemies/FSM/ScriptableObjects/Actions/ChaseAction.cs
+++ Assets/Scripts/Enemies/FSM/ScriptableObjects/Actions/ChaseAction.cs
@@ -7,10 +7,11 @@ public class ChaseAction : Action
 {
     private bool isPulsating;
     private Material myMat;
-
+    private Color baseColor;
     public override void Act(StateController controller)
     {
         myMat = controller.GetComponentInChildren<SkinnedMeshRenderer>().material;
+        baseColor = myMat.GetColor("_EmissiveColor");
         isPulsating = true;
         Chase(controller);
     }
@@ -18,7 +19,7 @@ public class ChaseAction : Action
     //Chase the player until he's dead by setting its Nav Mesh Agent destination to the player
     private void Chase(StateController controller)
     {
-        StartPulsating(100,635,4.5f,2);
+        StartPulsating(100,1000,4.5f,2);
         controller.animator.SetBool("Chasing", true);
         controller.animator.SetBool("Absorb", false);
         controller.navMeshAgent.isStopped = false;
@@ -30,8 +31,12 @@ public class ChaseAction : Action
         if (isPulsating)
         {
             float emission = Mathf.Lerp(minIntensity, maxIntensity, Mathf.PingPong(Time.time * pulsateSpeed, pulsateMaxDistance));
-
+            myMat.SetColor("_EmissiveColor", Color.red);
             myMat.SetFloat("_EmissiveIntensity", emission);
+        }
+        else
+        {
+            myMat.SetColor("_EmissiveColor", baseColor);
         }
     }
 }
