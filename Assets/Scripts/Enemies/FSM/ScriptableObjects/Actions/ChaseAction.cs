766bde1cb5531b7bad6cf88d2db996f8e286f53c
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-06-19 11:38:01 +0200

    Wip Pulsating on trashmob

--- Assets/Scripts/Enemies/FSM/ScriptableObjects/Actions/ChaseAction.cs
+++ Assets/Scripts/Enemies/FSM/ScriptableObjects/Actions/ChaseAction.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "PluggableAI/Actions/Chase")]
 public class ChaseAction : Action
 {
+    private bool isPulsating;
+    private Material myMat;
+
     public override void Act(StateController controller)
     {
+        myMat = controller.GetComponentInChildren<SkinnedMeshRenderer>().material;
+        isPulsating = true;
         Chase(controller);
     }
 
     //Chase the player until he's dead by setting its Nav Mesh Agent destination to the player
     private void Chase(StateController controller)
     {
+        StartPulsating(100,635,4.5f,2);
         controller.animator.SetBool("Chasing", true);
         controller.animator.SetBool("Absorb", false);
         controller.navMeshAgent.isStopped = false;
         controller.navMeshAgent.destination = controller.chaseTarget.position;
         //Debug.Log(controller.chaseTarget);
+    }
+    void StartPulsating(float minIntensity, float maxIntensity, float pulsateSpeed, float pulsateMaxDistance)
+    {
+        if (isPulsating)
+        {
+            float emission = Mathf.Lerp(minIntensity, maxIntensity, Mathf.PingPong(Time.time * pulsateSpeed, pulsateMaxDistance));
+
+            myMat.SetFloat("_EmissiveIntensity", emission);
+        }
     }
 }
