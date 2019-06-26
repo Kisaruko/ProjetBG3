356a7bf4994f5ee91074f9216691c714f39d8076
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-06-26 18:08:01 +0200

    Modification corruption behaviour, add corruption with dissolve, modif scrollingtexture

--- Assets/Scripts/Interactables/Visuals/CorruptionBehaviour.cs
+++ Assets/Scripts/Interactables/Visuals/CorruptionBehaviour.cs
@@ -19,11 +19,26 @@ public class CorruptionBehaviour : MonoBehaviour
     public GameObject PurificationVfx;
     private bool hasStartGrowing;
     private Transform lightObject;
+    public float timeToDissolve;
+
+    [Header("Scrolling Variables")]
+    public float newScrollX;
+    public float newScrollY;
 
     private bool isPurified;
+    private ScrollingTexture scrollingtexture;
+    private float baseScrollX;
+    private float baseScrollY;
+
     private void Start()
     {
         lightObject = FindObjectOfType<LightDetection>().transform;
+        if(GetComponent<ScrollingTexture>() != null)
+        {
+            scrollingtexture = GetComponent<ScrollingTexture>();
+            baseScrollX = scrollingtexture.scrollXSpeed;
+            baseScrollY = scrollingtexture.scrollYSpeed;
+        }
     }
 
     private void Update()
@@ -36,16 +51,29 @@ public class CorruptionBehaviour : MonoBehaviour
             }
         }
     }
+
     public void Shrinking()
     {
-        transform.DOScale(minScale, shrinkingTime);
+       // transform.DOScale(minScale, shrinkingTime);
+        if(scrollingtexture != null)
+        {
+            DOTween.To(() => scrollingtexture.scrollXSpeed, x => scrollingtexture.scrollXSpeed = x, newScrollX, shrinkingTime);
+            DOTween.To(() => scrollingtexture.scrollYSpeed, x => scrollingtexture.scrollYSpeed = x, newScrollY, shrinkingTime);
+            scrollingtexture.SetSpeed();
+        }
         this.gameObject.GetComponent<MeshRenderer>().material.DOColor(Color.black, shrinkingTime);
         hasStartGrowing = false;
     }
 
     public void Growing()
     {
-        transform.DOScale(maxScale, GrowingTime);
+      //  transform.DOScale(maxScale, GrowingTime);
+        if (scrollingtexture != null)
+        {
+            DOTween.To(() => scrollingtexture.scrollXSpeed, x => scrollingtexture.scrollXSpeed = x, baseScrollX, GrowingTime);
+            DOTween.To(() => scrollingtexture.scrollYSpeed, x => scrollingtexture.scrollYSpeed = x, baseScrollY, GrowingTime);
+            scrollingtexture.SetSpeed();
+        }
         this.gameObject.GetComponent<MeshRenderer>().material.DOColor(Color.white, GrowingTime);
 
         hasStartGrowing = true;
@@ -55,8 +83,7 @@ public class CorruptionBehaviour : MonoBehaviour
     {
         isPurified = true;
         hasStartGrowing = true;
-        transform.DOScale(0.1f, shrinkingTime /purificationShrinkDivider);
-
+        GetComponent<MeshRenderer>().material.DOFloat(1, "_Amount", timeToDissolve);
         if (PurificationVfx != null)
         {
             Instantiate(PurificationVfx, transform.position,Quaternion.identity);
