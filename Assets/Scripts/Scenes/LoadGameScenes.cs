c82d8f5ab5b404debb7204c7d599fe5136d03e50
Kerian <kerian.simon@e-artsup.net>
2019-07-02 11:10:11 +0200

    add menu play and quit - working on fade between scenes + options

---
+++ Assets/Scripts/Scenes/LoadGameScenes.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class LoadGameScenes : MonoBehaviour
+{
+
+    public List<string> scenesGameToLoad;
+    private IEnumerator coroutine;
+
+    private bool loadScenes = false;
+    public TextMeshProUGUI loadingText;
+    public Slider loadingSlider;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        coroutine = LoadScenesAsync(scenesGameToLoad);
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator LoadScenesAsync(List<string> scenesToLoad)
+    {
+        Scene originalScene = SceneManager.GetActiveScene();
+
+        List<AsyncOperation> sceneLoads = new List<AsyncOperation>();
+
+        for (int i = 0; i < scenesToLoad.Count; i++)
+        {
+            AsyncOperation sceneLoading = SceneManager.LoadSceneAsync(scenesToLoad[i], LoadSceneMode.Additive);
+            sceneLoading.allowSceneActivation = false;
+            sceneLoads.Add(sceneLoading);
+            while (sceneLoads[i].progress < 0.9f)
+            {
+                yield return null;
+            }
+        }
+
+        for (int i = 0; i < sceneLoads.Count; ++i)
+        {
+            sceneLoads[i].allowSceneActivation = true;
+            while (!sceneLoads[i].isDone)
+            {
+                yield return null;
+            }
+        }
+
+        AsyncOperation sceneUnloading = SceneManager.UnloadSceneAsync(originalScene);
+        while (!sceneUnloading.isDone)
+        {
+            yield return null;
+        }
+    }
+}
