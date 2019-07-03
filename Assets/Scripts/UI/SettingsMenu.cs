15d7fa530e473adfc5c5c00e85e6e2119b03ad29
Kerian <kerian.simon@e-artsup.net>
2019-07-03 14:56:32 +0200

    add menus + build inside project

---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public TMP_Dropdown resolutionDropdown; 
+    Resolution[] resolutions;
+
+    public PostProcessProfile m_profile;
+    ColorGrading m_colorGrading;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        m_colorGrading = m_profile.GetSetting<ColorGrading>();
+    }
+
+    public void SetGamma(float gamma)
+    {
+        m_colorGrading.gamma.value = new Vector4(0f, 0f, 0f, gamma);
+        Debug.Log(m_colorGrading.gamma.value);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
+    }
+}
