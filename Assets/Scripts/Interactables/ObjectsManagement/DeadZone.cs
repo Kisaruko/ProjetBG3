89d6d4325e0672ff198eb790845868087f6da291
Pierre\pzuca <pierre.zucalli@e-artsup.net>
2019-07-05 14:57:59 +0200

    Update deadzone to kill player, add it in will see

--- Assets/Scripts/Interactables/ObjectsManagement/DeadZone.cs
+++ Assets/Scripts/Interactables/ObjectsManagement/DeadZone.cs
@@ -6,6 +6,7 @@ public class DeadZone : MonoBehaviour
 {
     public List<string> activationTag;
     public bool mustDestroy = true;
+    public GameObject vfxHit;
     private void OnTriggerEnter(Collider other)
     {
         if (mustDestroy)
@@ -19,7 +20,15 @@ public class DeadZone : MonoBehaviour
                 }
                 else
                 {
-                    Initiate.Fade("GameOver", Color.black, 0.8f);
+                    if (vfxHit != null)
+                    {
+                        Instantiate(vfxHit, other.transform.position, Quaternion.identity);
+                    }
+                    GameManager.ShowAnImpact(0.3f);
+                    CameraShake.Shake(0.1f, 0.2f);
+                    other.GetComponentInParent<PlayerMovement>().Recoil(transform, 3f);
+                    other.GetComponentInParent<BinaryLight>().TakeHit();
+                    Initiate.Fade("GameOver",Color.black, 0.8f);
                 }
             }
         }
